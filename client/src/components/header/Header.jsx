cdbb1e7a4160a63671e0bf7df5a71b0e13048e61
PC <kadirkvl@gmail.com>
2023-04-16 13:11:53 +0300

    add header component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SearchOutlined, HomeOutlined } from '@ant-design/icons';
+import { SearchOutlined, HomeOutlined, ShoppingCartOutlined, CopyOutlined, UserOutlined, BarChartOutlined, LogoutOutlined} from '@ant-design/icons';
 import { Input } from 'antd';
 
 function Header() {
@@ -14,10 +14,30 @@ function Header() {
             <div className="header-search flex-1">
             <Input className='rounded-full max-w-[800px]' size="large" placeholder="Ürün ara.." prefix={<SearchOutlined/>} />
             </div>
-            <div className="menu-links">
+            <div className="menu-links flex justify-between items-center gap-8">
                 <a href='/' className='menu-link flex flex-col'>
                     <HomeOutlined className='md:text-2xl text-xl'/>
-                    <span className='md:text-xs text-[10px]'>Home</span>
+                    <span className='md:text-xs text-[10px]'>Anasayfa</span>
+                </a>
+                <a href='/' className='menu-link flex flex-col'>
+                    <ShoppingCartOutlined className='md:text-3xl text-2xl'/>
+                    <span className='md:text-xs text-[10px]'>Sepet</span>
+                </a>
+                <a href='/' className='menu-link flex flex-col'>
+                    <CopyOutlined className='md:text-2xl text-xl'/>
+                    <span className='md:text-xs text-[10px]'>Faturalar</span>
+                </a>
+                <a href='/' className='menu-link flex flex-col'>
+                    <UserOutlined className='md:text-2xl text-xl'/>
+                    <span className='md:text-xs text-[10px]'>Müşteriler</span>
+                </a>
+                <a href='/' className='menu-link flex flex-col'>
+                    <BarChartOutlined className='md:text-2xl text-xl'/>
+                    <span className='md:text-xs text-[10px]'>İstatislikler</span>
+                </a>
+                <a href='/' className='menu-link flex flex-col'>
+                    <LogoutOutlined className='md:text-2xl text-xl'/>
+                    <span className='md:text-xs text-[10px]'>Anasayfa</span>
                 </a>
             </div>
         </header>
